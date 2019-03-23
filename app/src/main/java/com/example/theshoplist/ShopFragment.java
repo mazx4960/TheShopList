17ca78bab9b1ea15dea0e91c4a20616c132ee9cd
bongbonglemon <gordonlim214@gmail.com>
2019-03-23 19:38:46 +0800

    Some addActivity implementations

--- app/src/main/java/com/example/theshoplist/ShopFragment.java
+++ app/src/main/java/com/example/theshoplist/ShopFragment.java
@@ -1,11 +1,14 @@
 package com.example.theshoplist;
 
 
+import android.arch.persistence.room.Room;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.theshoplist.SQL.ItemDatabase;
 
 
 /**
@@ -13,6 +16,7 @@ import android.view.ViewGroup;
  */
 public class ShopFragment extends Fragment {
 
+    ItemDatabase db;
 
     public ShopFragment() {
         // Required empty public constructor
@@ -22,8 +26,11 @@ public class ShopFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        db = Room.databaseBuilder(getContext(), ItemDatabase.class, "ItemsDB").allowMainThreadQueries().build();
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_shop, container, false);
     }
+
 
 }

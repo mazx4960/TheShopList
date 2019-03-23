fbf4a7de6f1f5cec1d295814dc8aa11d751986df
bongbonglemon <gordonlim214@gmail.com>
2019-03-23 18:52:29 +0800

    Initial commit

---
+++ app/src/main/java/com/example/theshoplist/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.theshoplist;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fm.beginTransaction();
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    fragmentTransaction.replace(R.id.container, new ShopFragment()).commit();
+                    return true;
+                case R.id.navigation_own:
+                    fragmentTransaction.replace(R.id.container, new OwnFragment()).commit();
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.add(R.id.container, new ShopFragment()).commit();
+    }
+
+
+
+}
